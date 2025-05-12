12bf27f3c4d9bfae8e3f1260b5259433ec355810
Nguyễn Hồng Sơn <mac2019@192.168.0.100>
2025-05-13 02:21:22 +0700

    fix

--- src/main/java/com/hls/sunflower/configuration/MyCorsConfig.java
+++ src/main/java/com/hls/sunflower/configuration/MyCorsConfig.java
@@ -15,7 +15,7 @@ public class MyCorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // Cho phép các domain cụ thể
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://elsun.site");
 
         // Hoặc cho phép tất cả các domain (không khuyến khích trong môi trường sản xuất)
         // config.addAllowedOrigin("*");
